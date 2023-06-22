85f166290649cfaa06fc46882abdb7644df893f2
Aiuuna_06610 <bimbazhapovaa@icloud.com>
2023-06-22 14:15:17 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp1/TodoItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+    public class TodoDO
+    {
+        public static TodoItem ConvertToEntity(
+            TodoItem
+            TodoItemDTO)
+        {
+            TodoItem Td = new TodoItem();
+            Td.Id = TodoItemDTO.Id;
+            Td.Name = TodoItemDTO.Name;
+            Td.IsComplete = TodoItemDTO.IsComplete;
+            return Td;
+        }
+    }
+}
